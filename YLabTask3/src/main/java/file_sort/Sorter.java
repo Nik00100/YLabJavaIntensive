def8af28be75dee9e38ca49370bce8f09a59c8b6
kirillovnick <kirillovnick@mail.ru>
2023-03-18 13:22:03 +0300

    corrections for Sorter

--- YLabTask3/src/main/java/file_sort/Sorter.java
+++ YLabTask3/src/main/java/file_sort/Sorter.java
@@ -5,78 +5,106 @@ import java.util.*;
 
 public class Sorter {
     // размер промежуточного файла
-    private static final int CHUNK_SIZE = 100000000;
+    private static final int CHUNK_SIZE = 10000000;
+    private static final String TMP_PREFIX = "temporary";
 
     public File sortFile(File dataFile) throws IOException {
-        /// Создаем временную папку для хранения промежуточных файлов.
-        File tmpDir = new File("temporary");
-        if (!tmpDir.exists()) {
-            tmpDir.mkdir();
-        }
+        List<File> chunks = createChunks(dataFile);
+        File output = new File("src/main/resources/sorted");
+        mergeSort(chunks, output);
+        deleteTempFiles(chunks);
+        return output;
+    }
 
-        // Разбиваем исходный файл на несколько файлов меньшего размера.
-        List<File> chunkFiles = new ArrayList<>();
+    private List<File> createChunks(File dataFile) throws IOException {
+        List<File> chunks = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(dataFile))) {
             String line;
-            int chunkNum = 0;
-            List<Long> chunk = new ArrayList<>();
+            List<Long> chunkData = new ArrayList<>();
+            int chunkCount = 0;
             while ((line = reader.readLine()) != null) {
-                chunk.add(Long.parseLong(line));
-                if (chunk.size() == CHUNK_SIZE) {
-                    // Отсортировать и записать промежуточный файл.
-                    Collections.sort(chunk);
-                    File chunkFile = new File(tmpDir, "chunk" + chunkNum + ".txt");
+                chunkData.add(Long.parseLong(line));
+                if (chunkData.size() == CHUNK_SIZE) {
+                    chunkData.sort(Long::compare);
+                    File chunkFile = File.createTempFile(TMP_PREFIX, null);
                     try (PrintWriter writer = new PrintWriter(chunkFile)) {
-                        for (Long number : chunk) {
-                            writer.println(number);
-                        }
+                        chunkData.forEach(writer::println);
                     }
-                    chunkFiles.add(chunkFile);
-                    chunkNum++;
-                    chunk.clear();
+                    chunks.add(chunkFile);
+                    chunkData.clear();
+                    chunkCount++;
                 }
             }
-            if (!chunk.isEmpty()) {
-                // Отсортировать и записать промежуточный файл.
-                Collections.sort(chunk);
-                File chunkFile = new File(tmpDir, "chunk" + chunkNum + ".txt");
+            if (!chunkData.isEmpty()) {
+                chunkData.sort(Long::compare);
+                File chunkFile = File.createTempFile(TMP_PREFIX, null);
                 try (PrintWriter writer = new PrintWriter(chunkFile)) {
-                    for (Long number : chunk) {
-                        writer.println(number);
-                    }
+                    chunkData.forEach(writer::println);
                 }
-                chunkFiles.add(chunkFile);
+                chunks.add(chunkFile);
             }
         }
+        return chunks;
+    }
 
-        // Объединяем отсортированные файлы в один большой отсортированный файл.
-        File sortedFile = new File("src/main/resources/sorted.txt");
-        PriorityQueue<Scanner> heap = new PriorityQueue<>(Comparator.comparingLong(scanner -> scanner.nextLong()));
-        for (File chunkFile : chunkFiles) {
-            Scanner scanner = new Scanner(new FileInputStream(chunkFile));
-            if (scanner.hasNextLong()) {
-                heap.offer(scanner);
-            }
+    private void mergeSort(List<File> chunks, File outputFile) throws IOException {
+        List<BufferedReader> readers = new ArrayList<>();
+        for (File chunk : chunks) {
+            readers.add(new BufferedReader(new FileReader(chunk)));
         }
-        try (PrintWriter writer = new PrintWriter(sortedFile)) {
-            while (!heap.isEmpty()) {
-                Scanner scanner = heap.poll();
-                long number = scanner.nextLong();
-                writer.println(number);
-                if (scanner.hasNextLong()) {
-                    heap.offer(scanner);
-                } else {
-                    scanner.close();
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            PriorityQueue<ChunkReader> queue = new PriorityQueue<>();
+            for (BufferedReader reader : readers) {
+                ChunkReader chunkReader = new ChunkReader(reader);
+                if (chunkReader.hasNext()) {
+                    queue.add(chunkReader);
                 }
             }
+            while (!queue.isEmpty()) {
+                ChunkReader chunkReader = queue.poll();
+                writer.println(chunkReader.next());
+                if (chunkReader.hasNext()) {
+                    queue.add(chunkReader);
+                }
+            }
+        }
+    }
+
+    private void deleteTempFiles(List<File> files) {
+        for (File file : files) {
+            file.delete();
+        }
+    }
+
+    private static class ChunkReader implements Comparable<ChunkReader> {
+        private final BufferedReader reader;
+        private String currentLine;
+
+        public ChunkReader(BufferedReader reader) {
+            this.reader = reader;
+            try {
+                this.currentLine = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
-        // Удаляем временные файлы.
-        for (File chunkFile : chunkFiles) {
-            chunkFile.delete();
+        public boolean hasNext() {
+            return currentLine != null;
         }
-        tmpDir.delete();
 
-        return sortedFile;
+        public long next() throws IOException {
+            long result = Long.parseLong(currentLine);
+            currentLine = reader.readLine();
+            if (currentLine == null) {
+                reader.close();
+            }
+            return result;
+        }
+
+        @Override
+        public int compareTo(ChunkReader o) {
+            return Long.compare(Long.parseLong(currentLine), Long.parseLong(o.currentLine));
+        }
     }
 }
--- YLabTask3/src/main/java/file_sort/Test.java
+++ YLabTask3/src/main/java/file_sort/Test.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        File dataFile = new Generator().generate("src/main/resources/data.txt", 10);
+        File dataFile = new Generator().generate("src/main/resources/data.txt", 100);
         System.out.println(new Validator(dataFile).isSorted()); // false
         File sortedFile = new Sorter().sortFile(dataFile);
         System.out.println(new Validator(sortedFile).isSorted()); // true
